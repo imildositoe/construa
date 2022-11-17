6070cbb7d6f0b9a1697c24909aec784f59327b7c
poisonshade <imildositoe@gmail.com>
2022-11-17 21:56:41 +0200

    imports

--- app/src/main/java/com/example/construa/selected_projecto/MaterialFragment.kt
+++ app/src/main/java/com/example/construa/selected_projecto/MaterialFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.construa.R
-import com.example.construa.BuildConfig
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
